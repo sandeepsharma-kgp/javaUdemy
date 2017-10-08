75010105ed5e2212b7bc791f07e446e40d2921fb
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2017-10-08 18:57:39 +0530

    Encapsulation v1

--- Encapsulation/src/com/javaUdemy/Main.java
+++ Encapsulation/src/com/javaUdemy/Main.java
@@ -4,17 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Player player = new Player();
-        player.name = "Tim";
-        player.health = 20;
-        player.weapon = "Sword";
+//        Player player = new Player();
+//        player.name = "Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining Health: "+ player.healthRemain());
+//
+//        damage = 12;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining Health: "+ player.healthRemain());
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining Health: "+ player.healthRemain());
-
-        damage = 12;
-        player.loseHealth(damage);
-        System.out.println("Remaining Health: "+ player.healthRemain());
+        EnhancedPlayer enhancedPlayer = new EnhancedPlayer("Tim ", 250, "Sword");
+        System.out.println("Initial health is: "+ enhancedPlayer.getHealth());
     }
 }
